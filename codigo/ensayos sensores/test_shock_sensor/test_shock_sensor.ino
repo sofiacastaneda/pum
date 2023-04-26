0f9a31436e278d7902972a58aea9ef2fd886e266
s/c <47247552+sofiacastaneda@users.noreply.github.com>
2023-04-26 13:05:09 -0500

    ua ua bom bom

---
+++ codigo/ensayos sensores/test_shock_sensor/test_shock_sensor.ino
@@ -0,0 +1,20 @@
+int sensorPin = A0; // define sensor pin
+int threshold = 500; // set threshold value
+
+void setup() {
+  Serial.begin(9600); // initialize serial communication
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin); // read sensor value
+  Serial.print("Sensor value: ");
+  Serial.println(sensorValue);
+
+  if (sensorValue > threshold) {
+    // do something when shock is detected
+    Serial.println("Shock detected!");
+    delay(100); // wait for a second to avoid multiple detection
+  }
+
+  delay(10); // delay between readings
+}
